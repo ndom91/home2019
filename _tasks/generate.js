4092cdd3005c8ad2f369d8ecaac6b5898b07812d
ndo@ndo1 <yo@ni.co.de>
2020-02-21 10:33:24 +0100

    update: gulp task typo

--- _tasks/generate.js
+++ _tasks/generate.js
@@ -1,13 +1,13 @@
-const gulp = require('gulp')
-const exec = require('child_process').exec'')
+const gulp = require("gulp");
+const exec = require("child_process").exec();
 
-require('dotenv').config()
+require("dotenv").config();
 
 const eleventy = cb => {
-    const cmd = `NODE_ENV=${process.env.NODE_ENV} eleventy`
-    exec(cmd, err => {
-        cb(err)
-    })
-}
+  const cmd = `NODE_ENV=${process.env.NODE_ENV} eleventy`;
+  exec(cmd, err => {
+    cb(err);
+  });
+};
 
-gulp.task('generate', eleventy)
+gulp.task("generate", eleventy);
